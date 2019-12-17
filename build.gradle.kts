323b56592eae16f6e689a16b3316298c3180686f
Lahres-Dev <lahres.dev@gmail.com>
2019-12-17 22:42:06 +0100

    minor updates to structure. jar now with correct manifest.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,12 @@ plugins {
 group = "com.github.lahresdev"
 version = "1.0-SNAPSHOT"
 
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.assertj:assertj-core:3.12.0")
+}
+
 repositories {
     mavenCentral()
     mavenLocal()
@@ -26,8 +32,13 @@ tasks.withType<Test> {
     finalizedBy("jacocoTestReport")
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("junit:junit:4.12")
-    testImplementation("org.assertj:assertj-core:3.12.0")
+tasks.withType<Jar> {
+    manifest {
+        attributes(mapOf("Main-Class" to "com.github.lahresdev.kgsm.HelloWorldKt"))
+    }
+    configurations["compileClasspath"].forEach { file: File -> from(zipTree(file.absoluteFile)) }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
